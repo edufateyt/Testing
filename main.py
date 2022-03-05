ab50ab897b26b5eabbfa98ef42745e6a17ddc70c
Vikrant Ojha <101007681+TheMalware@users.noreply.github.com>
2022-03-05 23:41:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import asyncio
 from pyrogram import filter, Client
 
-Client = Client("my_account")
-@Client.on_message(filters.private)
-async def hello(Client, message ):
-    await message.reply("Hello")
+Flux = Client("Test",
+               api_id=config.API_ID,
+               api_hash=config.API_HASH,
+               bot_token=config.BOT_TOKEN
+          )
+
+@Flux.on_message(filters.command(["start"]))
+async def hello(client, message):
+    await update.reply_text(text="Hello Brother")
     
-Client.run()
+Flux.run()
