61683b4e3c0237017e241458cb8f5133a57c4db9
Vikrant Ojha <101007681+TheMalware@users.noreply.github.com>
2022-03-06 11:21:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,20 +11,5 @@ Flux = Client("Test",
 @Flux.on_message(filters.command(["start"]))
 async def hello(Client, message):
     await message.reply_text(
-      text="👋 Hi There!\n\nThis Bot Is Made For Testing Purposes 🌀\n\n If You Want To Contribute, Help The Developer In Learning Pyrogram 🛠️.",
-      reply_markup = InlineKeyboardMarkup(
-      [
-        [
-          InlineKeyboardButton("Click", callback_data = "testing")
-        ]
-      ]
-      
-  )
-      
-@Flux.on_callback_query()
-async def button(Flux: Client,cmd: CallbackQuery):
-data = cmd.data
-if "testing" in data:
-await CallbackQuery.message.edit("Thanks For Trying")
-      
+      text="👋 Hi There!\n\nThis Bot Is Made For Testing Purposes 🌀\n\n If You Want To Contribute, Help The Developer In Learning Pyrogram 🛠️.")
 Flux.run()
