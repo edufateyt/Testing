7f5cca17b284b30a6a88891c6b0ee3329e6b8c8b
Vikrant Ojha <101007681+TheMalware@users.noreply.github.com>
2022-03-07 15:58:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ Flux = Client("Test",
                api_hash=Config.API_HASH,
                bot_token=Config.BOT_TOKEN
           )
-
-START_TXT = """👋 Hi There!\n\nThis Bot Is Made For Testing Purposes 🌀\n\n If You Want To Contribute, Help The Developer In Learning Pyrogram 🛠."""
 @Flux.on_message(filters.command(["start"]))
 async def start(Client, message):
   await message.reply_text(
@@ -17,9 +15,15 @@ async def start(Client, message):
     disable_web_page_preview =True,
     reply_markup = InlineKeyboardMarkup(
       [
-        [InlineKeyboardButton('Developer', url='t.me/TheMalwareAwakens'), InlineKeyboardButton('Bots', url='t.me/TheMalwareZone')]
+        [InlineKeyboardButton('Developer', url='t.me/TheMalwareAwakens'), InlineKeyboardButton('Bots', url='t.me/TheMalwareZone')],
+        [InlineKeyboardButton('Custom', callback_data='custom')]
       ]
     )
   )
+@Flux.on_callback_query()
+async def button(Client, CallbackQuery):
+  cb_data = CallbackQuery.data
+  if 'custom in cb_data:
+  await CallbackQuery.message.edit(text= "works perfectly")
   
 Flux.run()
