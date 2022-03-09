1c907d310472fdfa91dfdb7d257af6f8b52822b7
Vikrant Ojha <101007681+TheMalware@users.noreply.github.com>
2022-03-09 11:58:08 +0530

    Update force_sub.py

--- database/force_sub.py
+++ database/force_sub.py
@@ -5,17 +5,17 @@ from pyrogram import Client
 from pyrogram.errors import FloodWait, UserNotParticipant
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
-async def get_invite_link(bot : Client, chat_id: Union[str, int]):
+async def get_invite_link(Client, chat_id: Union[str, int]):
     try:
         invite_link = await Client.create_chat_invite_link(chat_id=chat_id)
         return invite_link
     except FloodWait as e:
         print(f"Sleep of {e.x}s caused by FloodWait ...")
         await asyncio.sleep(e.x)
-        return await get_invite_link(bot, chat_id)
+        return await get_invite_link(Client, chat_id)
 
 
-async def handle_force_sub(bot: Client, cmd: Message):
+async def handle_force_sub(Client, Message):
     if Config.UPDATES_CHANNEL and Config.UPDATES_CHANNEL.startswith("-100"):
         channel_chat_id = int(Config.UPDATES_CHANNEL)
     elif Config.UPDATES_CHANNEL and (not Config.UPDATES_CHANNEL.startswith("-100")):
@@ -23,10 +23,10 @@ async def handle_force_sub(bot: Client, cmd: Message):
     else:
         return 200
     try:
-        user = await bot.get_chat_member(chat_id=channel_chat_id, user_id=cmd.from_user.id)
+        user = await Client.get_chat_member(chat_id=channel_chat_id, user_id=Message.from_user.id)
         if user.status == "kicked":
-            await bot.send_message(
-                chat_id=cmd.from_user.id,
+            await Client.send_message(
+                chat_id=Message.from_user.id,
                 text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/FluxSupport).",
                 parse_mode="markdown",
                 disable_web_page_preview=True
@@ -34,12 +34,12 @@ async def handle_force_sub(bot: Client, cmd: Message):
             return 400
     except UserNotParticipant:
         try:
-            invite_link = await get_invite_link(bot, chat_id=channel_chat_id)
+            invite_link = await get_invite_link(Client, chat_id=channel_chat_id)
         except Exception as err:
             print(f"Unable to do Force Subscribe to {Config.UPDATES_CHANNEL}\n\nError: {err}")
             return 200
-        await bot.send_message(
-            chat_id=cmd.from_user.id,
+        await Client.send_message(
+            chat_id=Message.from_user.id,
             text="**Please Join My Updates Channel to use this Bot!**\n\n"
                  "**Due to Overload, Only Channel Subscribers can use the Bot!**",
             reply_markup=InlineKeyboardMarkup(
@@ -56,8 +56,8 @@ async def handle_force_sub(bot: Client, cmd: Message):
         )
         return 400
     except Exception:
-        await bot.send_message(
-            chat_id=cmd.from_user.id,
+        await Client.send_message(
+            chat_id=Message.from_user.id,
             text="Something went Wrong. Contact my [Support Group](https://t.me/FluxSupport).",
             parse_mode="markdown",
             disable_web_page_preview=True
