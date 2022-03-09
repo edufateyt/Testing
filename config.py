a19d85702cf8c84b2d2dabd67d148c109c48d36c
Vikrant Ojha <101007681+TheMalware@users.noreply.github.com>
2022-03-09 11:40:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,3 +6,4 @@ class Config(object):
     DATABASE_URL = os.environ.get("DATABASE_URL")
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL")
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+    BOT_USERNAME = os.environ.get("BOT_USERNAME")
