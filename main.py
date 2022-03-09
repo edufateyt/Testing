9d3bbcc2e03a62ce301cd9dbd6be999570166089
Vikrant Ojha <101007681+TheMalware@users.noreply.github.com>
2022-03-09 12:01:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ async def start(Client, message):
     if usr_cmd == "/start":
         await add_user_to_db(Client, Message)
         await Message.reply_text(
-            text="**👋 Hi There!\n\nThis Bot Is Made For Testing Purposes.\n\n If You Want To Contribute, Help The Developer In Learning Pyrogram.**",
+            text="**👋 Hi There!\n\nThis Bot Is Made For Testing Purposes.\n\n If You Want To Contribute, Help The Developer In Learning Pyrogram.**".format(Message.from_user.first_name, Message.from_user.id),
             parse_mode="Markdown",
             disable_web_page_preview=True,
             reply_markup = InlineKeyboardMarkup(
