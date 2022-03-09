4eca75d0d059f28e816ebf8d19fcea0120465583
Vikrant Ojha <101007681+TheMalware@users.noreply.github.com>
2022-03-09 11:04:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,6 @@ class Config(object):
     API_ID = int(os.environ.get("API_ID", ""))
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    DATABASE_URL = os.environ.get("DATABASE_URL")
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL")
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
